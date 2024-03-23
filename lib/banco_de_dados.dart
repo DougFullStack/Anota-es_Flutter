9bafd222554567b2f1a31b3015e019e5caca5b00
douglas <dublandoacena@gmail.com>
2024-03-22 22:32:50 -0300

    Singleton

---
+++ lib/banco_de_dados.dart
@@ -0,0 +1,9 @@
+class Banco {
+  static final Banco _banco = Banco._();
+
+  factory Banco() {
+    return _banco;
+  }
+
+  Banco._() {}
+}
--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -5,6 +5,55 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    TextEditingController _controleTitulo = TextEditingController();
+    TextEditingController _controleDescricao = TextEditingController();
+
+    void _exibirTelaCadastro() {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("Adicionar Nota"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: _controleTitulo,
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                        labelText: "Título",
+                        hintText: "Digite o título aqui..."),
+                  ),
+                  TextField(
+                    controller: _controleDescricao,
+                    decoration: const InputDecoration(
+                        labelText: "Descrição",
+                        hintText: "Digite a descrição aqui..."),
+                  )
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text("Cancelar")),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Salvar"))
+              ],
+            );
+          });
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Anotações"), centerTitle: true),
+      body: Container(),
+      backgroundColor: Colors.lightGreen,
+      floatingActionButton: FloatingActionButton(
+          onPressed: _exibirTelaCadastro,
+          foregroundColor: Colors.white,
+          child: const Icon(Icons.add)),
+    );
   }
 }
